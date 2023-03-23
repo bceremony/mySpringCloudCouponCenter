9bf006760c9d49cd37b2f1be2f71ad6c22b910c0
blake <blake@bytedance.com>
2023-03-23 22:39:01 +0800

    feat: add seata configuration

---
+++ coupon-template-serv/coupon-template-impl/src/main/java/com/geekbang/coupon/template/SeataConfiguration.java
@@ -0,0 +1,34 @@
+package com.geekbang.coupon.template;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * @author blake
+ * @version 1.0
+ * @description TODO
+ * @date 2023/3/23 22:35
+ */
+
+@Configuration
+public class SeataConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DruidDataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    @Bean("dataSource")
+    @Primary
+    public DataSource dataSourceDelegation(DruidDataSource druidDataSource) {
+        return new DataSourceProxy(druidDataSource);
+    }
+
+}
